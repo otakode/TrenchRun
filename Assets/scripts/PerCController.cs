2ceacb86ad2ed17c03509892498f01d24a5e4488
Otakode <marchal.cl@gmail.com>
2014-01-24 12:32:48 +0100

    add trenchrun
    Signed-off-by: Otakode <marchal.cl@gmail.com>

--- Assets/scripts/PerCController.cs
+++ Assets/scripts/PerCController.cs
@@ -1,15 +1,40 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PerCController : MonoBehaviour {
+public class PerCController : MonoBehaviour
+{
+	private PXCUPipeline pp;
 
-	// Use this for initialization
-	void Start () {
-	
+	private Vector3 leftHand;
+	private Vector3 rightHand;
+
+	void Start()
+	{
+		this.pp = new PXCUPipeline();
+		if (this.pp == null || !this.pp.Init(this.ppMode))
+			Debug.Log("PXCUPipeline Initialization ERROR");
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	void OnDestroy()
+	{
+		this.pp.Close();
+	}
+
+	void Update()
+	{
+		if (this.pp == null)
+			return;
+		PXCMGesture.GeoNode left;
+		PXCMGesture.GeoNode right;
+		this.pp.QueryGeoNode(PXCMGesture.GeoNode.Label.LABEL_BODY_HAND_LEFT, out left);
+		this.pp.QueryGeoNode(PXCMGesture.GeoNode.Label.LABEL_BODY_HAND_RIGHT, out right);
+		if (left.positionWorld.x < right.positionWorld.x)
+		{
+			PXCMGesture.GeoNode temp = left;
+			left = right;
+			right = temp;
+		}
+		this.leftHand = new Vector3(-left.positionWorld.x, left.positionWorld.z, -left.positionWorld.y);
+		this.rightHand = new Vector3(-right.positionWorld.x, right.positionWorld.z, -right.positionWorld.y);
 	}
 }
